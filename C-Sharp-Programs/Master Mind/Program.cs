bed4e596e9075b75106169d0553f5a15a217c516
Melinda Marino <melindamarino22@gmail.com>
2019-11-30 19:42:39 -0600

    Master Mind

---
+++ C-Sharp-Programs/Master Mind/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master_Mind
+{
+    class Program
+    {
+       static string[] colors = new string[] {"orange","pink", "red" };
+       static string color1;
+       static string color2;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Master Mind");
+            initColors();
+            askColors();
+            Console.ReadLine();
+        }
+        static void initColors()
+        {
+            Random rand = new Random();
+            int index1 = rand.Next(0, colors.Length);
+            int index2 = rand.Next(0, colors.Length);
+            color1 = colors[index1];
+            color2 = colors[index2];
+
+        }
+        static void askColors()
+        {
+            bool isWon = false;
+            while (!isWon)
+            {
+                Console.WriteLine("Enter your guess");
+                string guess = Console.ReadLine();
+                isWon = checkGuess(guess);
+            }
+        }
+        static bool checkGuess(string guess)
+        {
+            int colorsCorrect = 0;
+            int positionsCorrect = 0;
+            if(guess.Contains(color1))
+            {
+                colorsCorrect++;
+
+            }
+            if (guess.Contains(color2))
+            {
+                colorsCorrect++;
+
+            }
+            string[] parsedGuess = guess.Split(' ');
+            if (parsedGuess[0] == color1)
+            {
+                positionsCorrect++;
+            }
+            if (parsedGuess[1] == color2)
+            {
+                positionsCorrect++;
+            }
+            if (positionsCorrect == 2)
+            {
+                Console.WriteLine("You Won!!!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Your hint is: {colorsCorrect}-{positionsCorrect}");
+                return false;
+
+            }
+         
+        }
+    }
+}
--- C-Sharp-Programs/Towers Of Hanoi/Towers Of Hanoi/Program.cs
+++ C-Sharp-Programs/Towers Of Hanoi/Towers Of Hanoi/Program.cs
@@ -7,51 +7,60 @@ using System.Threading.Tasks;
 namespace Towers_Of_Hanoi
 {
     class Program
-    {
+    {    //this makes the pegs and makes them able to stack onto eachother
         static Dictionary<string, Stack<int>> pegs = new Dictionary<string, Stack<int>>();
         static void Main(string[] args)
-        {
+        {   //Writes the name of the game
             Console.WriteLine("This is the Towers Of Hanoi");
+            //these are the "tiles"
             int[] num = { 1, 2, 3, 4 };
 
-
+            //Calls the function initTower
             initTowers();
-
+            //Calls the function  printTowers
             printTowers();
+            //Calls the function playGame
             playGame();
-
+            // makes the imput readable
             Console.ReadKey();
 
         }
         static void printTowers()
-        {
+        {   //shows each peg name
             foreach (var name in pegs)
-            {
+            {   //writes how it will be displayed
                 Console.Write(name.Key + ":");
+                //sepparates each of the tiles to use
                 foreach (int value in name.Value)
                 {
+                    //writes the value
                     Console.Write(value.ToString());
                 }
+                //writes it out
                 Console.WriteLine();
             }
         }
         static void initTowers()
         {
+            //How to make a stack and the order its stacked in
             Stack<int> stack = new Stack<int>();
             stack.Push(4);
             stack.Push(3);
             stack.Push(2);
             stack.Push(1);
-
+            //the order the pegs are in, also being able to contain a stack
             pegs.Add("A", stack);
             pegs.Add("B", new Stack<int>());
             pegs.Add("C", new Stack<int>());
         }
         static bool isFinished()
         {
+            //
             Stack<int> stackC = pegs["C"];
+            //if it is true it applys
             if (stackC.Count == 4)
             { return true; }
+            //if its false then it will return false
             else
             { return false; }
         }
@@ -79,12 +88,13 @@ namespace Towers_Of_Hanoi
             }
         }
         static void move(string from, string to)
-        {
+        {   //if it can't move ftom and to
             if (!canMove(from, to))
-            {
+            {   //It will display an error messege
                 Console.WriteLine("Error, illegal move!");
                 return;
             }
+
             Stack<int> fromTower = pegs[from];
             Stack<int> toTower = pegs[to];
             int fromDisk = fromTower.Pop();
