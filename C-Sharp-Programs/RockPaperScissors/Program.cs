043664cb33381c493c36d0e641b789425c39813c
Melinda Marino <melindamarino22@gmail.com>
2019-12-18 02:22:40 -0600

    Rock, Paper, Scissors

--- C-Sharp-Programs/RockPaperScissors/Program.cs
+++ C-Sharp-Programs/RockPaperScissors/Program.cs
@@ -10,11 +10,92 @@ namespace RockPaperScissors
     {
         static void Main(string[] args)
         {
-            string rock = 
-            Random generator = new Random();
-            // creates a number 0,1 or 2
-            int randomNumber = generator.Next(0, 3);
-            static String CompareHands(String hand1, String hand2);
+            Console.WriteLine("Rock, Paper, or Scissors");
+            string userChoice = Console.ReadLine();
+
+            Random r = new Random();
+            int computerChoice = r.Next(4);
+
+            if (computerChoice == 1)
+            {
+                if (userChoice == "Rock")
+                {
+                    Console.WriteLine("The computer chose Rock");
+                    Console.WriteLine("It is a tie");
+                }
+                else if (userChoice == "Paper")
+                {
+                    Console.WriteLine("The computer chose Paper");
+                    Console.WriteLine("It is a tie");
+
+                }
+                else if (userChoice == "Scissors")
+                {
+                    Console.WriteLine("The computer chose Scissors");
+                    Console.WriteLine("It is a tie");
+                }
+                else
+                {
+                    Console.WriteLine("You must choose Rock, Paper, or Scissors!");
+
+                }
+
+            }
+
+            else if (computerChoice == 2)
+            {
+                if (userChoice == "Rock")
+                {
+                    Console.WriteLine("The computer chose paper");
+                    Console.WriteLine("Sorry you lose, Paper beat Rock");
+
+                }
+                else if (userChoice == "Paper")
+                {
+                    Console.WriteLine("The computer chose Scissors");
+                    Console.WriteLine("Sorry you lose, Scissors beat Paper ");
+
+                }
+                else if (userChoice == "Scissors")
+                {
+                    Console.WriteLine("The computer chose Rock");
+                    Console.WriteLine("Sorry you lose, Rock beats Scissors");
+                }
+                else
+                {
+                    Console.WriteLine("You must choose Rock, Paper, or Scissors!");
+                }
+            }
+            else if (computerChoice == 3)
+            {
+                if (userChoice == "Rock")
+                {
+                    Console.WriteLine("The computer chose Scissors");
+                    Console.WriteLine("You win, Rock beats Scissors");
+
+                }
+                else if (userChoice == "Paper")
+                {
+                    Console.WriteLine("The computer chose Rock");
+                    Console.WriteLine("You win, Paper beats Rock");
+
+                }
+                else if (userChoice == "Scissors")
+                {
+                    Console.WriteLine("The computer chose Paper");
+                    Console.WriteLine("You win, Scissors beat Paper");
+
+                }
+                else
+                {
+                    Console.WriteLine("You must choose Rock, Paper, or Scissors!");
+
+                }
+
+            }
+
+            Console.ReadLine();
         }
     }
+    
 }
